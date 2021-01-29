99b2cba6557977831c9977d7cf75c3cd5d04c044
Justin Hohing <jnh0627@yahoo.com>
2021-01-28 22:26:12 -0500

    TMDB api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,3 @@
-<<<<<<< HEAD
-
-// function displayMovieInfo() {
-
-//     var movie = $("#movieName").val();
-//     var queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=5f315e32";
-
-//     // Creates AJAX call for the specific movie button being clicked
-//     $.ajax({
-//       url: queryURL,
-//       method: "GET"
-//     }).then(function(response) {
-//       console.log(response);
-
-//     });
-
-//   }
-
-//   //calls the displayMovieInfo function when the submit button is clicked
-//   $(document).on("click", "#search", displayMovieInfo);
-
-//ID for the movie 
-var search = document.querySelector("#search");
-var movieTitle = document.querySelector("#movieTitle");
-var moviePlot = document.querySelector("#moviePlot");
-var movieGenre = document.querySelector("#movieGenre");
-var movieRelease = document.querySelector("#movieRelease");
-var movieRuntime = document.querySelector("#movieRuntime");
-var movieImages = document.querySelector("#movieImages")
-var movieRate = document.querySelector("#movieRate");
-
-$("#search").on("click", function (event) {
-=======
 // function displayMovieInfo() {
 
 //     var movie = $("#movieName").val();
@@ -77,7 +44,6 @@ var movieImages = document.querySelector("#movieImages")
 var movieRate = document.querySelector("#movieRate");
 
 $("#search").on("click", function(event) {
->>>>>>> origin/main
 
     event.preventDefault();
 
@@ -86,7 +52,6 @@ $("#search").on("click", function(event) {
 
 
     $.ajax({
-<<<<<<< HEAD
         url: queryURL,
         method: "GET"
     })
@@ -119,25 +84,3 @@ $("#search").on("click", function(event) {
 
 
 });
-=======
-      url: queryURL,
-      method: "GET"
-    })
-    .then(function(response){
-      console.log(response);
-      movieTitle.textContent = response.Title;
-      movieGenre.textContent = response.Genre
-      moviePlot.textContent = response.Plot;  
-     //images
-      var html = `
-      <figure>
-        <img src="${response.Poster}" alt="${response.Title}"/>
-      </figure>
-      `;
-      movieRate.textContent = response.Rated;
-      movieRuntime.textContent = response.Runtime;
-      movieRelease.textContent = response.Released;
-      $('#movieImages').prepend(html);
-    });
-  });
->>>>>>> origin/main
