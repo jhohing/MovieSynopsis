d5b2085f8c6b5a2ec1b6a5f66e0b9682a6b65e22
Steven Woo <swoo9875@gmail.com>
2021-02-03 21:33:32 -0500

    made changes to the index.html and fix the search result

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,27 +17,11 @@
 //   //calls the displayMovieInfo function when the submit button is clicked
 //   $(document).on("click", "#search", displayMovieInfo);
 
-$("#search").on("click", function (event) {
-  event.preventDefault();
-
-  var movie = $("#movieName").val();
-  var queryURL = "https://api.themoviedb.org/3/movie/{movie_id}?api_key=<<api_key>>&language=en-US";
-
-  // Creates AJAX call for the specific movie button being clicked
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function (response) {
-    console.log(response);
-
-  });
-});
-
 //Search Result 
 $("#search").on("click", function (event) {
 
   event.preventDefault();
-
+  $("#movieResult").empty();
 
   var movie = $("#movieName").val();
   var queryURL;
@@ -111,6 +95,7 @@ $("#search").on("click", function (event) {
         getMovie.forEach(function (movie) {
           var listItem = document.createElement("li");
           listItem.textContent = movie;
+          // movieHist.appendChild(listItem);
           movieHist.appendChild(listItem);
       });
     });
